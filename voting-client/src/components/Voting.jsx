a987740d307dcbf89f18cedbfd1503676bc61db0
luhtonen <luhtonen@gmail.com>
2016-03-09 13:09:22 +0100

    Enabling React and react-hot-loader and creating first dummy React component

---
+++ voting-client/src/components/Voting.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Voting extends React.Component {
+  render() {
+    return (
+      <div>Voting</div>
+    )
+  }
+}
+
+export default Voting;
--- voting-client/webpack.config.js
+++ voting-client/webpack.config.js
@@ -1,14 +1,17 @@
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: [
-    './src/index.js'
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+    './src/index.jsx'
   ],
   module: {
     loaders: [{
       test: /\.jsx?$/,
       exclude: /node_modules/,
-      loader: 'babel'
+      loader: 'react-hot!babel'
     }]
   },
   resolve: {
@@ -20,8 +23,10 @@ module.exports = {
     filename: 'bundle.js'
   },
   devServer: {
-    contentBase: './dist'
+    contentBase: './dist',
+    hot: true
   }, plugins: [
+    new webpack.HotModuleReplacementPlugin(),
     new HtmlWebpackPlugin({
       template: 'node_modules/html-webpack-template/index.ejs',
       title: 'Voting',
