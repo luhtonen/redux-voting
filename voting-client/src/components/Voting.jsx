bbc9d47dfb8cb6eb74ee665a76ee36447da937d1
luhtonen <luhtonen@gmail.com>
2016-03-09 13:46:04 +0100

    Implementing first unit test to test element rendering and UI event processing

--- voting-client/src/components/Voting.jsx
+++ voting-client/src/components/Voting.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 
 class Voting extends React.Component {
+  getPair() {
+    console.log('getPair ', this.props.pair);
+    return this.props.pair || [];
+  }
+
   render() {
     return (
-      <div>Voting</div>
-    )
+      <div className="voting">
+        {this.getPair().map(entry =>
+          <button key={entry} onClick={() => this.props.vote(entry)}>
+            <h1>{entry}</h1>
+          </button>
+        )}
+      </div>
+    );
   }
 }
 
