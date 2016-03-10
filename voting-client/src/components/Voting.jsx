39b580f0194ca22fc59b8c994c14cd5c9b10c462
luhtonen <luhtonen@gmail.com>
2016-03-10 08:46:27 +0100

    Refactoring Voting component to simplify it logic by splitting in into 2 new Vote and Winner components

--- voting-client/src/components/Voting.jsx
+++ voting-client/src/components/Voting.jsx
@@ -1,36 +1,15 @@
 import React from 'react';
+import Winner from './Winner';
+import Vote from './Vote';
 
 class Voting extends React.Component {
-  getPair() {
-    console.log('getPair ', this.props.pair);
-    return this.props.pair || [];
-  }
-
-  isDisabled() {
-    return !!this.props.hasVoted;
-  }
-
-  hasVotedFor(entry) {
-    return this.props.hasVoted === entry
-  }
-
   render() {
-    return (
-      <div className="voting">
-        {this.props.winner ?
-          <div ref="winner">Winner is {this.props.winner}</div> :
-          this.getPair().map(entry =>
-          <button key={entry}
-                  disabled={this.isDisabled()}
-                  onClick={() => this.props.vote(entry)}>
-            <h1>{entry}</h1>
-            {this.hasVotedFor(entry) ?
-              <div className="label">Voted</div> :
-              null}
-          </button>
-        )}
-      </div>
-    );
+    return <div>
+      {this.props.winner ?
+        <Winner ref="winner" winner={this.props.winner} /> :
+        <Vote {...this.props} />
+      }
+    </div>
   }
 }
 
