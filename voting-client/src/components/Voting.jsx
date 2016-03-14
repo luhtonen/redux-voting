130b73e4b1847398d3790d8b17d3c14cf473b33c
luhtonen <luhtonen@gmail.com>
2016-03-14 16:19:51 +0100

    Immutable Data And Pure Rendering

--- voting-client/src/components/Voting.jsx
+++ voting-client/src/components/Voting.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import PureRenderMixin from 'react-addons-pure-render-mixin';
 import Winner from './Winner';
 import Vote from './Vote';
 
 class Voting extends React.Component {
+  constructor(props) {
+    super(props);
+    this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+  }
+
   render() {
     return <div>
       {this.props.winner ?
