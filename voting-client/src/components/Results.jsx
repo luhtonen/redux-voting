6187f4bf2d0e5a80593f43273a9fbfaee4d630e4
luhtonen <luhtonen@gmail.com>
2016-03-14 19:57:26 +0100

    Creating Results component

--- voting-client/src/components/Results.jsx
+++ voting-client/src/components/Results.jsx
@@ -1,15 +1,47 @@
 import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
 
+export const VOTE_WIDTH_PERCENT = 8;
+
 class Results extends React.Component {
   constructor(props) {
     super(props);
     this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
   }
 
+  getPair() {
+    return this.props.pair || [];
+  }
+
+  getVotes(entry) {
+    if (this.props.tally && this.props.tally.has(entry)) {
+      return this.props.tally.get(entry);
+    }
+    return 0;
+  }
+
+  getVotesBlockWidth(entry) {
+    return (this.getVotes(entry) * VOTE_WIDTH_PERCENT) + '%';
+  }
+
   render() {
     return (
-      <div>Results</div>
+      <div className="results">
+        <div className="tally">
+          {this.getPair().map(entry =>
+            <div key={entry} className="entry">
+              <h1>{entry}</h1>
+              <div className="voteVisualization">
+                <div className="votesBlock"
+                     style={{width: this.getVotesBlockWidth(entry)}}></div>
+              </div>
+              <div className="voteCount">
+                {this.getVotes(entry)}
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
     )
   }
 }
