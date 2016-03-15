e3f5780d49d3281bfad21db08a528ea9de3ad724
luhtonen <luhtonen@gmail.com>
2016-03-15 15:07:29 +0100

    Connect the client to the server

--- voting-client/src/index.jsx
+++ voting-client/src/index.jsx
@@ -13,17 +13,11 @@ import {ResultsContainer} from './components/Results';
 require('./style.css');
 
 const store = createStore(reducer);
-store.dispatch({
-  type: ActionTypes.SET_STATE,
-  state: {
-    vote: {
-      pair: ['Sunshine', '28 Days Later'],
-      tally: {Sunshine: 2}
-    }
-  }
-});
 
 const socket = io(`${location.protocol}//${location.hostname}:8090`);
+socket.on('state', state =>
+  store.dispatch({type: ActionTypes.SET_STATE, state})
+);
 
 const routes = <Route component={App}>
   <Route path="/results" component={ResultsContainer}/>
