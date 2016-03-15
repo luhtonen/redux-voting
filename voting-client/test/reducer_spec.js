89821de80e60e5713d42e51f221bfda20cffa04e
luhtonen <luhtonen@gmail.com>
2016-03-15 10:05:24 +0100

    Adding reducer's unit tests and simple reducer implementation

---
+++ voting-client/test/reducer_spec.js
@@ -0,0 +1,69 @@
+import {List, Map, fromJS} from 'immutable';
+import {expect} from 'chai';
+
+import reducer from '../src/reducer';
+import ActionTypes from '../src/actionTypes';
+
+describe('reducer', () => {
+  it('handles SET_STATE', () => {
+    const initalState = Map();
+    const action = {
+      type: ActionTypes.SET_ENTRIES,
+      state: Map({
+        vote: Map({
+          pair: List.of('Trainspotting', '28 Days Later'),
+          tally: Map({Trainspotting: 1})
+        })
+      })
+    };
+    const nextState = reducer(initalState, action);
+
+    expect(nextState).to.equal(fromJS({
+      vote: {
+        pair: ['Trainspotting', '28 Days Later'],
+        tally: {Trainspotting: 1}
+      }
+    }));
+  });
+
+  it('handles SET_STATE with plain JS payload', () => {
+    const initalState = Map();
+    const action = {
+      type: ActionTypes.SET_ENTRIES,
+      state: {
+        vote: {
+          pair: ['Trainspotting', '28 Days Later'],
+          tally: {Trainspotting: 1}
+        }
+      }
+    };
+    const nextState = reducer(initalState, action);
+
+    expect(nextState).to.equal(fromJS({
+      vote: {
+        pair: ['Trainspotting', '28 Days Later'],
+        tally: {Trainspotting: 1}
+      }
+    }));
+  });
+
+  it('handles SET_STATE without initial state', () => {
+    const action = {
+      type: ActionTypes.SET_ENTRIES,
+      state: {
+        vote: {
+          pair: ['Trainspotting', '28 Days Later'],
+          tally: {Trainspotting: 1}
+        }
+      }
+    };
+    const nextState = reducer(undefined, action);
+
+    expect(nextState).to.equal(fromJS({
+      vote: {
+        pair: ['Trainspotting', '28 Days Later'],
+        tally: {Trainspotting: 1}
+      }
+    }));
+  });
+});
